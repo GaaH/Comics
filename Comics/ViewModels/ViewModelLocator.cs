49514d54a28db6a0ec27b8f8870c872668ffe9f3
Gaëtan Caillaut <gaetan.caillaut@live.com>
2017-05-18 17:36:17 +0200

    ViewModelLocator first implementation

---
+++ Comics/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,39 @@
+﻿using Comics.Views;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Views;
+using Microsoft.Practices.ServiceLocation;
+using System;
+
+namespace Comics.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            if (ViewModelBase.IsInDesignModeStatic)
+            {
+
+            }
+            else
+            {
+                SimpleIoc.Default.Register(BuildNavigationService);
+            }
+        }
+
+        private INavigationService BuildNavigationService()
+        {
+            var service = new NavigationService();
+            var pageTypes = new Type[] { typeof(MainPage) };
+
+            foreach (var pageType in pageTypes)
+            {
+                service.Configure(pageType.Name, pageType);
+            }
+
+            return service;
+        }
+    }
+}
