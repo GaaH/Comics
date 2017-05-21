fef2ed43fd3593fa2c1daaff756625045f3d7917
Gaëtan Caillaut <gaetan.caillaut@live.com>
2017-05-21 20:10:28 +0200

    Moved Load*ComicCommand to ComicViewModel

--- Comics/ViewModels/ComicViewModel.cs
+++ Comics/ViewModels/ComicViewModel.cs
@@ -27,15 +27,41 @@ namespace Comics.ViewModels
         }
 
         public abstract ICommand ResumeComicCommand { get; }
-        public abstract RelayCommand LoadOldestComicCommand { get; }
-        public abstract RelayCommand LoadLatestComicCommand { get; }
-        public abstract RelayCommand LoadPreviousComicCommand { get; }
-        public abstract RelayCommand LoadNextComicCommand { get; }
+        public RelayCommand LoadOldestComicCommand { get; }
+        public RelayCommand LoadLatestComicCommand { get; }
+        public RelayCommand LoadPreviousComicCommand { get; }
+        public RelayCommand LoadNextComicCommand { get; }
 
         public ComicViewModel(INavigationService navigationService, IComicProvider comicProvider)
         {
             NavigationService = navigationService;
             ComicProvider = comicProvider;
+
+            LoadOldestComicCommand = new RelayCommand(LoadOldestComicAsync, () => Comic == null || Comic.HasPreviousComic);
+            LoadLatestComicCommand = new RelayCommand(LoadLatestComicAsync, () => Comic == null || Comic.HasNextComic);
+
+            LoadPreviousComicCommand = new RelayCommand(LoadPreviousComicAsync, () => Comic != null && Comic.HasPreviousComic);
+            LoadNextComicCommand = new RelayCommand(LoadNextComicAsync, () => Comic != null && Comic.HasNextComic);
+        }
+
+        private async void LoadOldestComicAsync()
+        {
+            Comic = await ComicProvider.LoadOldestComicAsync();
+        }
+
+        private async void LoadLatestComicAsync()
+        {
+            Comic = await ComicProvider.LoadLatestComicAsync();
+        }
+
+        private async void LoadPreviousComicAsync()
+        {
+            Comic = await ComicProvider.LoadComicAsync(Comic.PreviousComic);
+        }
+
+        private async void LoadNextComicAsync()
+        {
+            Comic = await ComicProvider.LoadComicAsync(Comic.NextComic);
         }
     }
 }
